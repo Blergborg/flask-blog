febbb1222fa82a3ef854f4aaf6bd3b3cd545507a
Phil Prater <pprater92@gmail.com>
2020-04-15 12:15:52 -0700

    password reset emails. trouble with gmail auth.

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,6 +7,8 @@ load_dotenv(find_dotenv())
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-
+    MAIL_SERVER= os.getenv("MAIL_SERVER")
+    MAIL_PORT= os.getenv("MAIL_PORT")
+    MAIL_USE_TLS= os.getenv("MAIL_USE_TLS")
+    MAIL_USERNAME= os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD= os.getenv("MAIL_PASSWORD")
